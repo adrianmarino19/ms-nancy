36ec59270fba1bac03c4fb94e06881dab17d8b85
Adrian Marino <adrian.marino@ailylabs.com>
2026-07-12 14:46:16 +0200

    feat: playing with tools hand-coding last time

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
+from typing import Any
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
@@ -10,8 +12,7 @@ client = OpenAI(
     api_key=API_KEY
 )
 
-# name = input("What is your name? ")
-
+# name = input("What is your name? ") 
 
 def llm_call(
     text: list,
@@ -32,17 +33,46 @@ def llm_call(
 
 tools = [
     {
+        "type": "function",
         "name": "get_name",
         "parameters": None,
-        "type": "function",
         "description": "Get the user's name.",
         "strict": False,
+    },
+    {
+        "type": "function",
+        "name": "sumdrian",
+        "description": "Get the sumdrian of two numbers. Sumdrian is a special sum in the Sumdrian planet.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "x": {
+                    "type": "integer",
+                    "description": "first integer to add",
+                },
+                "y": {
+                    "type": "integer",
+                    "description": "second integer to add",
+                },
+            },
+            "required": ["x", "y"],
+            "additionalProperties": False,
+        },
+        "strict": True,
     }
-]    
+]   
+
 
 def get_name():
     return "The name of him is Adrian. He is your Master"
 
+def sumdrian(x, y):
+    """
+    Calculates the sumdrian of two numbers. 
+    Sumdrian is an alternative addition in the Sumdrian planet.
+    """
+    sum = x + y
+    return f"{sum}-drians"
 
 
 def conversation():
@@ -56,9 +86,32 @@ def conversation():
             previous_id,
             tools
         )
-        print(output)
-        text = input()
-    
+        if output[0].type == "function_call":
+            if output[0].name == "get_name":
+                tool_result = get_name()
+                history.append(
+                    {
+                        "type": "function_call_output",
+                        "call_id": output[0].call_id,
+                        "output": tool_result,
+                    })
+            if output[0].name == "sumdrian":
+                args = json.loads(output[0].arguments)
+                x = args["x"]
+                y = args["y"]
+                tool_result = sumdrian(x, y)
+                
+                history.append(
+                    {
+                        "type": "function_call_output",
+                        "call_id": output[0].call_id,
+                        "output": tool_result,
+                    })
+        else: 
+            print(output_text)
+            text = input()
+            history = [{"role": "user", "content": text}]
+
 
 if __name__ == "__main__":
     conversation()
